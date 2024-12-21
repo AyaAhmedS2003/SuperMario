5aac25c330472eae0f02a436ee196d281feb326f
Aya Abdelmuniem <>
2024-12-21 04:17:46 +0200

    create consts

---
+++ src/CONST/Consts.java
@@ -0,0 +1,22 @@
+package CONST;
+
+
+public class Consts {
+    public static int y = 15;
+    public static int maxWidth = 100;
+    public static int maxHeight = 100;
+    public static String textureNames[] = {
+            "back-in.jpeg",
+            "mario1.png",
+            "mario2.png",
+            "mario3.png",
+            "mario1.png",
+            "bullet.png"
+    };
+
+    public static String assetsFolderName = "Assets/Mario";
+
+
+}
+
+
