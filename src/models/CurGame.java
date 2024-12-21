db32bbda3fdaacc93c995490e6a0390465a28b42
Aya Abdelmuniem <>
2024-12-21 04:20:52 +0200

    Create Game

---
+++ src/models/CurGame.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author asus
+ */
+public class CurGame {
+
+    public boolean paused;
+    public long Timer;
+    public String firstPlayerName;
+    public String secondPlayerName;
+    public boolean isMultipalyer;
+    private int currentPlayer;
+    private int firstPlayerLives;
+    private int secondPlayerLives;
+    private int secondPlayerScore;
+    private int firstPlayerScore;
+    public int marioIdx = 1;// mario idx is between {1, 2, 3, 4}
+    public int xBullet;
+    public boolean fired = false;
+    public int xMario;
+
+    public String getCurrentPlayerName() {
+        if (currentPlayer == 1) {
+            return firstPlayerName;
+        }
+        return secondPlayerName;
+    }
+
+    public int getCurrentPlayerScore() {
+        if (currentPlayer == 1) {
+            return firstPlayerScore;
+        }
+        return secondPlayerScore;
+    }
+
+    public void setCurrentPlayerScore(int score) {
+        if (currentPlayer == 1) {
+            firstPlayerScore = score;
+        } else {
+            secondPlayerScore = score;
+        }
+    }
+
+    public int getCurrentPlayerLives() {
+        if (currentPlayer == 1) {
+            return firstPlayerLives;
+        }
+        return secondPlayerLives;
+    }
+
+    public void setCurrentPlayerLives(int score) {
+        if (currentPlayer == 1) {
+            firstPlayerLives = score;
+        } else {
+            secondPlayerLives = score;
+        }
+    }
+
+    public void newGame() {
+        paused = false;
+        isMultipalyer = false;
+        Timer = 0;
+        firstPlayerScore = 0;
+        secondPlayerScore = 0;
+        firstPlayerLives = 3;
+        secondPlayerLives = 3;
+        xMario = 10;
+    }
+}
--- src/models/GameObj.java
+++ src/models/GameObj.java
@@ -1,1 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
 
+/**
+ *
+ * @author asus
+ */
+public class GameObj {
+    public int x;
+    public String type;
+    public boolean remove;
+
+    void GameObj(int x, String type) {
+        this.x = x;
+        this.type = type;
+    }
+}
